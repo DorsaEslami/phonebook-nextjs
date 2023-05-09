38bd590964d9871b663f1c342d926dc1c60a83b1
DorsaEslami <dorsa.eslami97@gmail.com>
2023-05-10 01:57:58 +0330

    edited middleware

--- phonebook/middleware.ts
+++ phonebook/middleware.ts
@@ -1,9 +1,18 @@
 import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    var booleanToken: boolean = !!req.nextauth.token?.token;
+    if ((req.nextUrl.pathname.startsWith("/dashboard") || req.nextUrl.pathname.startsWith("/dashboard")) && !booleanToken)
+      return NextResponse.rewrite(
+        new URL("/", req.url)
+      );
+  },
   {
     callbacks: {
-      authorized: ({ token }) => !!token?.token,
+      authorized: ({ token }) => !!token,
     },
   }
 );
