3a1f4df1f1aba6521aeb855139210dc1853c8925
DorsaEslami <dorsa.eslami97@gmail.com>
2023-05-09 17:40:37 +0330

    Callback edited in middleware

--- phonebook/middleware.ts
+++ phonebook/middleware.ts
@@ -3,17 +3,19 @@ import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth(
-  // function middleware(req) {
-  //   if (req.nextUrl.pathname.startsWith("/dashboard") && !!req.nextauth.token) {
-  //     return NextResponse.rewrite(new URL("/auth/login?message=You Are Not logged in!", req.url));
-  //   }
-  //   if (req.nextUrl.pathname.startsWith("/changePassword") && !!req.nextauth.token) {
-  //     return NextResponse.rewrite(new URL("/auth/login?message=You Are Not logged in!", req.url));
-  //   }
-  // },
+  function middleware(req) {
+    console.log(!req.nextauth.token)
+    if (req.nextUrl.pathname.startsWith("/dashboard") && !req.nextauth.token) {
+      return NextResponse.rewrite(new URL("/auth/login?message=You Are Not logged in!", req.url));
+    }
+    if (req.nextUrl.pathname.startsWith("/changePassword") && !req.nextauth.token) {
+      return NextResponse.rewrite(new URL("/auth/login?message=You Are Not logged in!", req.url));
+    }
+  },
   {
     callbacks: {
-      authorized: ({ token }) => !!token,
+      authorized: ({ token }) => !!token
+
     },
   }
 );
